5c689df1e8cc5cae117d0766262899e23350ecc5
lerelynmoraleda25 <lerelyn.moraleda10@gmail.com>
2013-11-27 09:53:34 +0800

    Create home.php

---
+++ home.php
@@ -0,0 +1,20 @@
+Web-Prog2
+=========
+<html>
+
+	<center>
+	<h1>post messages</h1>
+
+	<form method = "POST" action = "postmessage.php">
+		<table border>
+			<tr>
+		<td>Name: <input type = "text" name = "name"><br></td></tr>
+		<td>Email: <input type = "text" name = "email"><br></td></tr>
+		<td>Message:<br><textarea name  = "message"></textarea><br><br></td></tr>
+		<td><input type = "submit" value = "POST MESSAGE"></td></tr>
+		</tr>
+	</table>
+	</form>
+</center>
+</html>
+                            
