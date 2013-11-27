3e3d4ffd142ca16f032867f1de1e92026a447ec3
lerelynmoraleda25 <lerelyn.moraleda10@gmail.com>
2013-11-27 09:54:38 +0800

    Create post.php

---
+++ post.php
@@ -0,0 +1,16 @@
+<?php 
+	include "utilities.php";
+	
+	if(!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['message'])){
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+		$message = $_POST['message'];
+		$action = 'N';
+
+		post_message($name,$email,$message,$action);
+		header("location:showmess.php");
+	}else{
+		header("location:home.php");
+	}
+	
+?>
